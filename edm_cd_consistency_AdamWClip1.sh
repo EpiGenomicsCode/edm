e1129fcd60ce7e2205521188e8a872e997883f9b
arora123you <31950798+arora123you@users.noreply.github.com>
2026-03-05 13:29:55 -0500

    bash resume again

--- edm_cd_consistency_AdamWClip1.sh
+++ edm_cd_consistency_AdamWClip1.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=72
 #SBATCH --mem=0
-#SBATCH --time=24:00:00
+#SBATCH --time=30:00:00
 #SBATCH --output=/u/xyou1/edm/slurm_logs/%x-%j.out
 #SBATCH --error=/u/xyou1/edm/slurm_logs/%x-%j.err
 
@@ -56,7 +56,7 @@ export LOCAL_RANK=0
 
 # Run training with torchrun and arguments per prompt
 torchrun --standalone --nproc_per_node=4 train.py \
-  --outdir=/u/xyou1/edm/training-runs/imagenet64-cd-s8/AdamWClip1 \
+  --outdir=/work/hdd/bbse/xyou1/edm/training-runs/imagenet64-cd-s8/AdamWClip1 \
   --data=/work/nvme/bbse/vmathew/edm_training/edm/datasets/imagenet-64x64.zip \
   --cond=1 --arch=adm --precond=edm \
   --batch=2048 --batch-gpu=64 --fp16=True --ema=50 --lr=8e-5 --ema_rampup=0.05 --grad-clip=1.0 \
@@ -82,4 +82,4 @@ torchrun --standalone --nproc_per_node=4 train.py \
   --val_at_start=0 \
   --dropout=0.0 \
   --seed=1959836853 \
-  --resume=/work/nvme/bbse/xyou1/training-runs/imagenet64-cd-s8/AdamWClip1/00000-imagenet-64x64-cond-adm-edm-gpus4-batch2048-fp16-cdS8-T64-1280/training-state-006146.pt
+  --resume=/u/xyou1/edm/training-runs/imagenet64-cd-s8/AdamWClip1/00001-imagenet-64x64-cond-adm-edm-gpus4-batch2048-fp16-cdS8-T64-1280/training-state-012292.pt
